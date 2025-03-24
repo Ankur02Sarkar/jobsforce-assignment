d3d2f68b0d2797ba70f337ede6922b6f3d770b90
Ankur02Sarkar <sarkarrohonankur@gmail.com>
2025-03-24 23:37:31 +0530

    feat(public): add favicon and related icons for web app manifest and PWA

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
 import type { NextConfig } from "next";
+// import withPWAInit from "@ducanh2912/next-pwa";
+
+// const withPWA = withPWAInit({
+//   dest: "public",
+// });
 
 const nextConfig: NextConfig = {
   /* config options here */
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono, Outfit, Poppins } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -24,9 +24,44 @@ const outfit = Outfit({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
+const defaultUrl = "https://jobs-force.vercel.app";
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
   title: "JobSource AI",
   description: "AI Powered Mock Interview System",
+  icons: {
+    icon: [
+      {
+        url: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
+    apple: [{ url: "/apple-touch-icon.png" }],
+  },
+  appleTouchIcon: "/apple-touch-icon.png",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+    startupImage: [
+      {
+        url: "/web-app-manifest-512x512.png",
+        media:
+          "(device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
+      },
+      {
+        url: "/web-app-manifest-192x192.png",
+        media:
+          "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
+      },
+    ],
+  },
+};
+
+export const viewport: Viewport = {
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
@@ -36,7 +71,22 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="JobSource AI" />
+        <link rel="manifest" href="/manifest.json" />
         <body
           className={`${poppins.variable} ${outfit.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
         >
