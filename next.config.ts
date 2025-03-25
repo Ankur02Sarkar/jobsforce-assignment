a4f10539b1f5e935ac35838d02d9240cc1d5ce1b
Ankur02Sarkar <sarkarrohonankur@gmail.com>
2025-03-25 10:45:50 +0530

    chore: update environment variables and dependencies
    Add NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY and CLERK_SECRET_KEY to .env.example. Include @radix-ui/react-slot and motion as new dependencies in package.json. Modify next.config.ts to support remote image patterns for Next.js.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
-// import withPWAInit from "@ducanh2912/next-pwa";
-
-// const withPWA = withPWAInit({
-//   dest: "public",
-// });
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
